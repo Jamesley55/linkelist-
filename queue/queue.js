14009bdf3715dc851a05e38de1d8696a4985f2e3
Jamesley55 <Jamesleyjoseph5@gmail.com>
2020-01-21 20:23:01 -0500

    qeue  and stack

---
+++ queue/queue.js
@@ -0,0 +1,66 @@
+class Node
+{
+
+    constructor(data)
+    {
+        this.data = data; 
+        this.next= null; 
+    }
+
+}
+
+
+class queue
+{
+    constructor()
+    {
+        this.head = null;
+        this.tail = null; 
+        this.size = 0 ;
+    }
+    add(element)
+    {
+        let node = new Node(element);
+
+        if(this.tail == null)
+        { 
+            this.head = node; 
+            this.tail = node; 
+            this.size++; 
+
+        }
+        else{
+            this.tail.next = node; 
+            this.tail = node; 
+            this.size++;
+            return this; 
+
+        }
+    }
+    unsift()
+    {
+        let node = this.head; 
+        this.head = node.next; 
+        node = null; 
+    }
+    sowQueue()
+    {
+        let node = this.head; 
+        while(node)
+        {
+            console.log(node.data);
+            node = node.next; 
+        }
+    }
+
+}
+
+let quue = new queue();
+
+var l =5 ;
+var t = 6; 
+var a = 7; 
+quue.add(l);
+quue.add(t);
+quue.add(a);
+quue.sowQueue(); 
---
+++ stacks/stack.js
@@ -0,0 +1,52 @@
+class stack 
+{
+    constructor()
+    {
+        this.item  =[]; 
+        this.size = 0; 
+    }
+    push(data)
+    {
+        this.item[this.size] = data ;
+        this.size++;
+    }
+    pop()
+    {
+        this.item[this.size] = null ; 
+        this.size++; 
+
+    }
+    peek()
+    {
+         return this.item[this.size];
+    }
+    printstack()
+    {
+        
+        while(this.size> 0)
+        {
+            console.log(this.item[this.size]);
+            this.size--; 
+        }
+    }
+}
+
+let stak = new stack();
+
+var dat = "a";
+var da = "ab";
+var d = "abb";
+var datt = "abb";
+var datttt = "asss";
+var dattt = "abbb";
+stak.push(dat);
+stak.push(da);
+stak.push(d);
+stak.push(datt);
+stak.push(dattt);
+stak.push(datttt);
+stak.push(datttt);
+stak.printstack(); 
+
+
+
